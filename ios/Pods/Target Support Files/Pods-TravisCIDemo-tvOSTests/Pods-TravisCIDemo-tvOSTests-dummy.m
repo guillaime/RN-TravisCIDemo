96952cacb2899c4f792b1cd4a92d44301bdcb5de
Guillaime <guillaime.janssen@gmail.com>
2019-03-11 11:38:41 +0100

    travis test #25

---
+++ ios/Pods/Target Support Files/Pods-TravisCIDemo-tvOSTests/Pods-TravisCIDemo-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TravisCIDemo_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_TravisCIDemo_tvOSTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-TravisCIDemoTests/Pods-TravisCIDemoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TravisCIDemoTests : NSObject
+@end
+@implementation PodsDummy_Pods_TravisCIDemoTests
+@end
