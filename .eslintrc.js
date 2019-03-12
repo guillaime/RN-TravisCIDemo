f884b31a5c3d5a437f22c9abcf4ad7bc9208c76c
Guillaime <guillaime.janssen@gmail.com>
2019-03-12 16:45:47 +0100

    travis test #33

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,9 @@ module.exports = {
         'no-use-before-define': 'off',
         'react/jsx-filename-extension': 'off',
         'react/prop-types': 'off',
-        'comma-dangle': 'off'
+        'comma-dangle': 'off',
+        'react/destructuring-assignment': 'always',
+        'react/sort-comp': [0, { }]
     },
     'globals': {
         "fetch": false
--- __tests__/App.js
+++ __tests__/App.js
@@ -5,22 +5,20 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
 
 import renderer from 'react-test-renderer';
-import {shallow} from "react-native/jest/renderer";
+import App from '../App';
 
 
 describe('Testing App Component', () => {
-
-  it('Generates correctly', () => {
+  it('It Generates', () => {
     renderer.create(<App />);
   });
 
-  it('Can generate an array from 10 to 1', () => {
-    let loginComponent = renderer.create(<App />).getInstance();
+  it('It can generate an array from 10 to 1', () => {
+    const loginComponent = renderer.create(<App />).getInstance();
     const array = loginComponent.generateArray();
 
-    expect(array).toEqual([10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
+    expect(array).toEqual([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]);
   });
 });
