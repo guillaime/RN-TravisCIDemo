f275c747732d6d7aa7c610e3e4cf3ae4ffe410ca
Guillaime <guillaime.janssen@gmail.com>
2019-03-13 15:16:49 +0100

    Styled App.js for deployment

--- App.js
+++ App.js
@@ -11,13 +11,28 @@ import React from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  Linking,
+  Image
 } from 'react-native';
 
 function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.instructions}>Welcome to this TravisCIDemo</Text>
+      <Image source={require('./assets/images/travislogo.png')} style={styles.image} />
+      <Text style={styles.title}>
+        Welcome to this TravisCIDemo
+      </Text>
+      <Text style={styles.instructions}>
+        This app is full automatically deployed with TravisCI and Fastlane!
+        For more information, please contact me!
+      </Text>
+      <Text
+        style={[styles.instructions, { color: 'blue', marginTop: 20 }]}
+        onPress={() => Linking.openURL('https://github.com/guillaime')}
+      >
+        Open GitHub
+      </Text>
     </View>
   );
 }
@@ -30,11 +45,28 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
     paddingTop: 50
   },
+  title: {
+    textAlign: 'center',
+    color: '#333333',
+    fontWeight: 'bold',
+    marginBottom: 10,
+    marginLeft: 20,
+    marginRight: 20,
+    fontSize: 22
+  },
   instructions: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+    marginLeft: 40,
+    marginRight: 40,
+    fontSize: 16
   },
+  image: {
+    width: 200,
+    height: 200,
+    marginBottom: 50
+  }
 });
 
 export default App;
