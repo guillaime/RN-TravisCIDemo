17c8cacefbb5a2cdcd043ef93ffccf5980b855eb
Guillaime <guillaime.janssen@gmail.com>
2019-03-13 11:00:04 +0100

    Updated version number

--- __tests__/App.js
+++ __tests__/App.js
@@ -14,11 +14,4 @@ describe('Testing App Component', () => {
   it('It Generates', () => {
     renderer.create(<App />);
   });
-
-  it('It can generate an array from 10 to 1', () => {
-    const loginComponent = renderer.create(<App />).getInstance();
-    const array = loginComponent.generateArray();
-
-    expect(array).toEqual([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]);
-  });
 });
